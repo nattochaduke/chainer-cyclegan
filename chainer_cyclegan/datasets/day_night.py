2e9319ab225fea48e2448b80012462c473e60d27
nattochaduke <mamada.n.aa@m.titech.ac.jp>
2019-04-09 11:51:42 +0900

    Change epoch length

--- chainer_cyclegan/datasets/day_night.py
+++ chainer_cyclegan/datasets/day_night.py
@@ -12,6 +12,9 @@ ROOT_DIR = chainer.dataset.get_dataset_directory('wkentaro/chainer-cyclegan')
 
 class DayNightDataset(UnpairedDirectoriesDataset):
 
-    def __init__(self, directory_a='../data/DarkFace_Unlabeled', directory_b='../data/train2014', split='train'):
+    def __init__(self, directory_a='./data/DarkFace_Unlabeled', directory_b='./data/train2014', split='train'):
 
         super(DayNightDataset, self).__init__(directory_a, directory_b, split)
+
+    def __len__(self):
+        return 720 # As coco is very large, we shorted the epochs
